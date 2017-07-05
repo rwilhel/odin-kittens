c43b255bf3e01661ec1fd37b2fd025c9fe1eae71
rwilhel <rwilhel@gmail.com>
2017-07-04 20:39:47 -0600

    Adds flash messages to the application for create, update & delete actions

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -17,10 +17,11 @@ class KittensController < ApplicationController
 
   def create
     @kitten = Kitten.new(kitten_params)
-
     if @kitten.save
+      flash[:success] = "Check out that brand new kitten!"
       redirect_to @kitten
     else
+      flash.now[:error] = "Are you kidding me with that? Try again."
       render 'new'
     end
   end
@@ -29,8 +30,10 @@ class KittensController < ApplicationController
     @kitten = Kitten.find(params[:id])
 
     if @kitten.update(kitten_params)
+      flash[:success] = "Check out that revamped kitten!"
       redirect_to @kitten
     else
+      flash.now[:error] = "Are you kidding me with that? Try again."
       render 'edit'
     end
   end
@@ -38,7 +41,7 @@ class KittensController < ApplicationController
   def destroy
     @kitten = Kitten.find(params[:id])
     @kitten.destroy
-
+    flash[:success] = "Bye bye, kitten!"
     redirect_to kittens_path
   end
 
